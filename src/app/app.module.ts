0ee6a8be78284f5aaf942cc6d41794318daa7ef4
JavCarFer <javcarfer92@gmail.com>
2020-07-30 18:25:21 +0200

    Routing complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { PaintsComponent } from './paints/paints.component';
+
+import { PaintService } from './paints/paint.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'paints', pathMatch:'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'paints', component: PaintsComponent },
+];
+
 
 @NgModule({
   declarations: [ // Registro de componentes
@@ -16,9 +27,10 @@ import { PaintsComponent } from './paints/paints.component';
     PaintsComponent
   ],
   imports: [ // Registro de módulos externos a la app
-    BrowserModule // Directivas ngIf, ngFor, trabajo con peticiones rest, etc...
+    BrowserModule, // Directivas ngIf, ngFor, trabajo con peticiones rest, etc...
+    RouterModule.forRoot(routes)
   ],
-  providers: [], // Registro de servicios
+  providers: [PaintService], // Registro de servicios
   bootstrap: [AppComponent] // Componente principal a cargar
 })
 export class AppModule { }
