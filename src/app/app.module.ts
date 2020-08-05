4140143b8f9638e48d0736829b0f578af9b9902b
JavCarFer <javcarfer92@gmail.com>
2020-08-05 11:04:48 +0200

    Adding form for paints and FormsModule to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+}
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,6 +13,7 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { PaintsComponent } from './paints/paints.component';
 
 import { PaintService } from './paints/paint.service';
+import { FormComponent } from './paints/form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'paints', pathMatch:'full' },
@@ -24,10 +28,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    PaintsComponent
+    PaintsComponent,
+    FormComponent
   ],
   imports: [ // Registro de módulos externos a la app
     BrowserModule, // Directivas ngIf, ngFor, trabajo con peticiones rest, etc...
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [PaintService], // Registro de servicios
--- src/app/paints/paint.service.ts
+++ src/app/paints/paint.service.ts
@@ -1,18 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Paint} from './paint';
 import { of, Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class PaintService {
-
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/paints';
+  constructor(private http: HttpClient) { }
 
   getPaints(): Observable<Paint[]>{
-    let paints: Paint[] = [
+  /*  let paints: Paint[] = [
       {id:1,version:0,name:'Heavy Warmgrey', brand:'Vallejo', code:'72.148'},
       {id:2,version:0,name:'Abaddon black', brand:'Citadel', code:'Base'},
       {id:3,version:0,name:'Bloodfest Crimson', brand:'Scale75', code:'SFG-11'}
     ];
     return of(paints);
+    */
+   return this.http.get<Paint[]>(this.urlEndPoint);
+   /*return this.http.get(this.urlEndPoint).pipe(
+     map( response => response as Paint[])
+   );
+   */
   }
 }
